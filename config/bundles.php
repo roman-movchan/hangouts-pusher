d9ffb57d97592781623576626c14d91c7efd6eb5
Roman Movchan <r.movchan@treeum.net>
2018-06-18 23:23:56 +0300

    init

---
+++ config/bundles.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Http\HttplugBundle\HttplugBundle::class => ['all' => true],
+];
---
+++ src/Controller/GitBucketController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class GitBucketController extends Controller
+{
+    /**
+     * @Route("/webhook/gitbucket", name="webhook_gitbucket")
+     * @Method("POST")
+     */
+    public function index(Request $request)
+    {
+        $content = $request->getContent();
+        $webHookUrl = 'https://chat.googleapis.com/v1/spaces/AAAARTJcHxs/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=d7ay9oT-T1C_P-Z1iMrVBUaU5bH30MPQNLuuLMlVT78%3D';
+        if (empty($content)) {
+            return $this->json([]);
+        }
+
+        $params = json_decode($content, true);
+        $commitsCount = sizeof($params['commits']);
+
+        $text = <<<TEXT
+There is new *$commitsCount* commit(s) in repo *{$params["repository"]["name"]}* \n\n
+TEXT;
+
+        foreach ($params['commits'] as $commit) {
+            $date = new \DateTime($commit['timestamp']);
+            $date = $date->format('d.m.Y H:i');
+            $url = $commit['url'];
+            $files = implode(', ', $commit['added']);
+            $text .= <<<TEXT
+_{$date}_ <{$url}|{$commit['message']}> 
+Files: {$files}        
+TEXT;
+        }
+
+        dump($text);
+        $body = [
+            'text' =>
+                $text
+        ];
+
+        $headers = [
+            'Content-Type' => 'application/json'
+        ];
+
+        $request = $this->container->get('httplug.message_factory')->createRequest('POST', $webHookUrl, $headers, json_encode($body));
+        $response = $this->container->get('httplug.client')->sendRequest($request);
+
+        return $this->json($response);
+    }
+
+
+
+}
