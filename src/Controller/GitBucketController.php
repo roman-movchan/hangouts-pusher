73d4f6afa7a5113b590e62e80a64bfd0e5c50909
Roman Movchan <r.movchan@treeum.net>
2018-06-21 21:25:04 +0300

    add key

--- src/Controller/GitBucketController.php
+++ src/Controller/GitBucketController.php
@@ -4,26 +4,38 @@ namespace App\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class GitBucketController extends Controller
 {
     /**
-     * @Route("/webhook/gitbucket", name="webhook_gitbucket")
+     * @Route("/webhook/{key}", name="webhook_gitbucket")
      * @Method("POST")
      */
-    public function index(Request $request, \Twig_Environment $twig)
+    public function index(Request $request, string $key)
     {
+        $webHookUrls = [
+            '9982ad58bdcbd6732604d451053ca5cd21f09da8' => 'https://chat.googleapis.com/v1/spaces/AAAARTJcHxs/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=d7ay9oT-T1C_P-Z1iMrVBUaU5bH30MPQNLuuLMlVT78%3D'
+        ];
+
+        if(!array_key_exists($key, $webHookUrls)) {
+           throw new HttpException(Response::HTTP_BAD_REQUEST, 'No key founded');
+        }
+
+        $url = $webHookUrls[$key];
+
         $content = $request->getContent();
-        $webHookUrl = 'https://chat.googleapis.com/v1/spaces/AAAARTJcHxs/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=d7ay9oT-T1C_P-Z1iMrVBUaU5bH30MPQNLuuLMlVT78%3D';
+
         if (empty($content)) {
             return $this->json([]);
         }
 
         $params = json_decode($content, true);
 
-        $text = $twig->render('git_message.html.twig', $params);
+        $text = $this->container->get('twig')->render('git_message.html.twig', $params);
 
         $body = [
             'text' =>
@@ -34,7 +46,7 @@ class GitBucketController extends Controller
             'Content-Type' => 'application/json'
         ];
 
-        $request = $this->container->get('httplug.message_factory')->createRequest('POST', $webHookUrl, $headers, json_encode($body));
+        $request = $this->container->get('httplug.message_factory')->createRequest('POST', $url, $headers, json_encode($body));
         $response = $this->container->get('httplug.client')->sendRequest($request);
 
         return $this->json($response);
